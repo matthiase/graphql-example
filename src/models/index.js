bebbfd28c4e27ab49a768bfaee9244977f4f4c13
Matthias Eder <nodeful@gmail.com>
2019-01-11 06:40:01 -0800

    Set up GraphQL example boilerplate.

---
+++ src/models/index.js
@@ -0,0 +1,30 @@
+let users = {
+  1: {
+    id: '1',
+    username: 'Alice Smith',
+    messageIds: [1]
+  },
+  2: {
+    id: '2',
+    username: 'Bob Jones',
+    messageIds: [2]
+  }
+}
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World!',
+    userId: '1'
+  },
+  2: {
+    id: '2',
+    text: 'This is a test.',
+    userId: '2'
+  }
+}
+
+export default {
+  users,
+  messages
+}
